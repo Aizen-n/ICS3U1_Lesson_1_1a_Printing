a6efc987f7a02ad337782cf46712b2f6fa92a51a
Aizen-n <javin.thomas24@ycdsbk12.ca>
2024-02-08 11:47:33 -0500

    Updated PrintingComments ja

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,14 +8,27 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
+    System.out.println("Hi");
+    System.out.println("There!");
 
     // Say hello on the same line
-    
+    System.out.print("Hi");
+    System.out.print(" There!");
     
     // multiline comment
+    /* 
+     * this is 
+     * a multi line
+     * comment
+     */
     
-    
-
+    // Draw a box
+    /* 
+    System.out.println("*************");
+    System.out.println("*           *");
+    System.out.println("*           *");
+    System.out.println("*           *");
+    System.out.println("*************");
+    */
   }
 }
